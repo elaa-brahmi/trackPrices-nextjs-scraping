19a4ebd85e1dd6483fec972c20dfdf375e358b40
elaa <elaa.brahmii@gmail.com>
2025-05-21 23:11:16 +0100

    dynamically rendering the product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
 import Image from 'next/image'
 import Searchbar from '../../components/Searchbar'
 import HeroCarousel from '../../components/HeroCarousel'
 import "./globals.css";
+import { getAllProducts } from '../../lib/actions/index'
+import ProductCard from '../../components/ProductCard';
 
-const home = () => {
+const home = async () => {
+  const allProducts=await getAllProducts();
+
   return (
     <>
     <section className="px-6 md:px-20 py-24">
@@ -34,10 +39,10 @@ const home = () => {
     <section className="trending-section">
       <h2 className="section-text">Trending</h2>
       <div className="flex flex-wrap gap-x-8 gap-y-16">
-        {['Apple iPhone 14 Pro Max','book','Sneakers'].map
+     {    allProducts.map
         ((product) => (
-          <div key={product}>{product}</div>
-        ))}
+         <ProductCard key={product._id} product={product}/>
+        ))} 
 
       </div>
 
